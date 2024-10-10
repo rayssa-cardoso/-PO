f0e80e3957ba35e981ac339c7e572cdc83722245
rayssa <rayssa.cardoso.gonc@gmail.com>
2024-10-10 11:52:05 -0300

    10/10

---
+++ po/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace po;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ po/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace po;
+
+public partial class MainPage : ContentPage
+{
+	 const int gravidade =1;
+	 const int TempoEntreFrames=25;
+	 bool estaMorto = false;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	 void AplicaGravidade()
+    { 
+	  cuphead.png.TranslationY+=gravidade;
+
+
+
+	}
+
+
+	}
+
+}
+
