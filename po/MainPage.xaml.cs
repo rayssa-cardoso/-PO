34ae0a65fec3a055b963425f38474f3e156afa6b
rayssa <rayssa.cardoso.gonc@gmail.com>
2024-10-17 11:47:14 -0300

    17/10

--- po/MainPage.xaml.cs
+++ po/MainPage.xaml.cs
@@ -2,12 +2,21 @@
 
 public partial class MainPage : ContentPage
 {
-	const int gravidade = 1;
+	const int gravidade = 5;
 	const int TempoEntreFrames = 25;
+	const int forcaPulo = 30;
+	const int maxTempoPulando = 3;
+	const int aberturaMinima = 100;
+
 	bool estaMorto = true;
-	double larguraJanela = 0;
-	double alturaJanela = 0;
+	bool estaPulando = false;
+	
 	int velocidade = 20;
+	int tempoPulando = 0;
+	
+	double LarguraJanela;
+	double AlturaJanela;
+	
 	public MainPage()
 	{
 		InitializeComponent();
@@ -18,72 +27,67 @@ public partial class MainPage : ContentPage
 		cuphed.TranslationY += gravidade;
 
 	}
-	protected override void OnAppearing()
-	{
-		base.OnAppearing();
-		Desenha();
-	}
+
 	public async void Desenha()
 	{
 		while (!estaMorto)
 		{
-			AplicaGravidade();
-			await Task.Delay(TempoEntreFrames);
 			GerenciarCanos();
+			if(estaPulando)
+				AplicaPulo();
+			else
+				AplicaGravidade();
+			if(VerificaColisao()){
+				estaMorto = true;
+				GameOverFrame.IsVisible = true;
+				break;
+			}
+			await Task.Delay(TempoEntreFrames);
 		}
 	}
 
-	void La(Object s, TappedEventArgs e)
-	{
-		GameOverFrame.IsVisible = false;
-		estaMorto = false;
-		Inicializar();
-		Desenha();
-	}
+	
 	void Inicializar()
 	{
 		cuphed.TranslationY = 0;
+		estaMorto = false;
+		FlorAlto.TranslationX=0;
+		FlorBaixo.TranslationX=0;
 	}
 
 	protected override void OnSizeAllocated(double w, double h)
 	{
 		base.OnSizeAllocated(w, h);
-		larguraJanela = w;
-		alturaJanela = h;
+		LarguraJanela = w;
+		AlturaJanela = h;
 	}
 
 	void GerenciarCanos()
 	{
 		FlorAlto.TranslationX -= velocidade;
 		FlorBaixo.TranslationX -= velocidade;
-		if(FlorBaixo.TranslationX < -larguraJanela){
+
+		if(FlorBaixo.TranslationX < -LarguraJanela)
+		{
 			FlorBaixo.TranslationX = 40;
 			FlorAlto.TranslationX = 40;
 		}
 	}
 
-	void La (object s , TappedEventArgs e)
+	void OnGameOverClicked (object s , TappedEventArgs e)
     {
-		GameOverFrame.IsVisible = 0;
-        estaMorto = false;
+		GameOverFrame.IsVisible = false ;
 		Inicializar();
 		Desenha();
-
 	}
     
-	void Inicializar()
-	{
-		cuphed.TranslationY = 0;
-	
-	}
-
 	bool VerificaColisaoTeto()
 	{ 
 		var minY=-AlturaJanela/2;
-		if (cuphed.TranslationY >= minY)
-		return true;
+		if (cuphed.TranslationY <= minY)
+			return true;
 		else
-		return false;
+			return false;
 	}
     
 	bool VerificaColisaoChao()
@@ -96,58 +100,32 @@ public partial class MainPage : ContentPage
 	}
     
 	bool VerificaColisao()
-	{ 
-		if
-
-
-
-
-	}
-	
-
-
-
-
+	{
+		if(!estaMorto)
+		{
+			if(VerificaColisaoTeto() || VerificaColisaoChao())
+		{
+			return true;
+		}
+		}
+		return false;
 	}
 
+	void AplicaPulo()
+	{
+		cuphed.TranslationY -= forcaPulo;
+		tempoPulando++;
 
-         
-
-
-
-
-	 }
-
-
-
-
+		if (tempoPulando >= maxTempoPulando)
+		{
+			estaPulando = false;
+			tempoPulando = 0;
+		}
 	}
 
-
-
-
-
-
-
-
-
+	void OnGridClicked(object s, TappedEventArgs args)
+	{
+		estaPulando = true;
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
 
